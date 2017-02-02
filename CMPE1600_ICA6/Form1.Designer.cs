2bc9a6c1ed86121f4e99db658011199a7fc91b2f
William Fichtner <william_25_35@hotmail.com>
2017-02-01 20:34:28 -0700

    finished, but pretty janky i.e no methods

--- CMPE1600_ICA6/Form1.Designer.cs
+++ CMPE1600_ICA6/Form1.Designer.cs
@@ -60,15 +60,17 @@
             // 
             // UI_DiceBox1
             // 
+            this.UI_DiceBox1.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.UI_DiceBox1.Location = new System.Drawing.Point(12, 35);
             this.UI_DiceBox1.Name = "UI_DiceBox1";
-            this.UI_DiceBox1.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox1.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox1.TabIndex = 0;
+            this.UI_DiceBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox1_MouseClick);
             // 
             // UI_RollButton
             // 
-            this.UI_RollButton.Location = new System.Drawing.Point(124, 102);
+            this.UI_RollButton.Location = new System.Drawing.Point(136, 95);
             this.UI_RollButton.Name = "UI_RollButton";
             this.UI_RollButton.Size = new System.Drawing.Size(75, 23);
             this.UI_RollButton.TabIndex = 1;
@@ -78,57 +80,69 @@
             // 
             // UI_PlayAgainButton
             // 
-            this.UI_PlayAgainButton.Location = new System.Drawing.Point(124, 194);
+            this.UI_PlayAgainButton.Enabled = false;
+            this.UI_PlayAgainButton.Location = new System.Drawing.Point(136, 189);
             this.UI_PlayAgainButton.Name = "UI_PlayAgainButton";
             this.UI_PlayAgainButton.Size = new System.Drawing.Size(75, 23);
             this.UI_PlayAgainButton.TabIndex = 2;
             this.UI_PlayAgainButton.Text = "Play Again";
             this.UI_PlayAgainButton.UseVisualStyleBackColor = true;
+            this.UI_PlayAgainButton.Click += new System.EventHandler(this.UI_PlayAgainButton_Click);
             // 
             // UI_DiceBox2
             // 
+            this.UI_DiceBox2.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.UI_DiceBox2.Location = new System.Drawing.Point(63, 35);
+            this.UI_DiceBox2.Location = new System.Drawing.Point(68, 35);
             this.UI_DiceBox2.Name = "UI_DiceBox2";
-            this.UI_DiceBox2.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox2.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox2.TabIndex = 3;
+            this.UI_DiceBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox2_MouseClick);
             // 
             // UI_DiceBox3
             // 
+            this.UI_DiceBox3.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.UI_DiceBox3.Location = new System.Drawing.Point(114, 35);
+            this.UI_DiceBox3.Location = new System.Drawing.Point(124, 35);
             this.UI_DiceBox3.Name = "UI_DiceBox3";
-            this.UI_DiceBox3.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox3.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox3.TabIndex = 4;
+            this.UI_DiceBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox3_MouseClick);
             // 
             // UI_DiceBox4
             // 
+            this.UI_DiceBox4.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.UI_DiceBox4.Location = new System.Drawing.Point(165, 35);
+            this.UI_DiceBox4.Location = new System.Drawing.Point(180, 35);
             this.UI_DiceBox4.Name = "UI_DiceBox4";
-            this.UI_DiceBox4.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox4.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox4.TabIndex = 5;
+            this.UI_DiceBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox4_MouseClick);
             // 
             // UI_DiceBox5
             // 
+            this.UI_DiceBox5.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.UI_DiceBox5.Location = new System.Drawing.Point(216, 35);
+            this.UI_DiceBox5.Location = new System.Drawing.Point(236, 35);
             this.UI_DiceBox5.Name = "UI_DiceBox5";
-            this.UI_DiceBox5.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox5.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox5.TabIndex = 6;
+            this.UI_DiceBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox5_MouseClick);
             // 
             // UI_DiceBox6
             // 
+            this.UI_DiceBox6.BackColor = System.Drawing.Color.White;
             this.UI_DiceBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.UI_DiceBox6.Location = new System.Drawing.Point(267, 35);
+            this.UI_DiceBox6.Location = new System.Drawing.Point(292, 35);
             this.UI_DiceBox6.Name = "UI_DiceBox6";
-            this.UI_DiceBox6.Size = new System.Drawing.Size(45, 45);
+            this.UI_DiceBox6.Size = new System.Drawing.Size(50, 50);
             this.UI_DiceBox6.TabIndex = 7;
+            this.UI_DiceBox6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_DiceBox6_MouseClick);
             // 
             // UI_RollLabel
             // 
             this.UI_RollLabel.AutoSize = true;
-            this.UI_RollLabel.Location = new System.Drawing.Point(121, 138);
+            this.UI_RollLabel.Location = new System.Drawing.Point(137, 138);
             this.UI_RollLabel.Name = "UI_RollLabel";
             this.UI_RollLabel.Size = new System.Drawing.Size(25, 13);
             this.UI_RollLabel.TabIndex = 8;
@@ -137,7 +151,7 @@
             // UI_ScoreLabel
             // 
             this.UI_ScoreLabel.AutoSize = true;
-            this.UI_ScoreLabel.Location = new System.Drawing.Point(121, 168);
+            this.UI_ScoreLabel.Location = new System.Drawing.Point(137, 168);
             this.UI_ScoreLabel.Name = "UI_ScoreLabel";
             this.UI_ScoreLabel.Size = new System.Drawing.Size(35, 13);
             this.UI_ScoreLabel.TabIndex = 9;
@@ -155,7 +169,7 @@
             // UI_RollValue
             // 
             this.UI_RollValue.AutoSize = true;
-            this.UI_RollValue.Location = new System.Drawing.Point(164, 138);
+            this.UI_RollValue.Location = new System.Drawing.Point(202, 138);
             this.UI_RollValue.Name = "UI_RollValue";
             this.UI_RollValue.Size = new System.Drawing.Size(13, 13);
             this.UI_RollValue.TabIndex = 10;
@@ -164,7 +178,7 @@
             // UI_ScoreValue
             // 
             this.UI_ScoreValue.AutoSize = true;
-            this.UI_ScoreValue.Location = new System.Drawing.Point(164, 168);
+            this.UI_ScoreValue.Location = new System.Drawing.Point(201, 168);
             this.UI_ScoreValue.Name = "UI_ScoreValue";
             this.UI_ScoreValue.Size = new System.Drawing.Size(13, 13);
             this.UI_ScoreValue.TabIndex = 11;
@@ -174,7 +188,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 261);
+            this.ClientSize = new System.Drawing.Size(355, 303);
             this.Controls.Add(this.UI_ScoreValue);
             this.Controls.Add(this.UI_RollValue);
             this.Controls.Add(this.UI_ScoreLabel);
--- CMPE1600_ICA6/Form1.cs
+++ CMPE1600_ICA6/Form1.cs
@@ -13,19 +13,26 @@ namespace CMPE1600_ICA6
 {
     public partial class Form1 : Form
     {
-        int dice1 = 0;
-        int dice2 = 0;
-        int dice3 = 0;
-        int dice4 = 0;
-        int dice5 = 0;
-        int dice6 = 0;
-        int rollCount = 0;
-        private SoundPlayer mPlayer = new SoundPlayer();
+        int dice1 = 0;  //
+        int dice2 = 0;  //
+        int dice3 = 0;  //
+        int dice4 = 0;  //  These store the value of the dice
+        int dice5 = 0;  //
+        int dice6 = 0;  //
+        bool dice1Hold = false; //
+        bool dice2Hold = false; //
+        bool dice3Hold = false; //
+        bool dice4Hold = false; //  These check if the dice should be held or not
+        bool dice5Hold = false; //
+        bool dice6Hold = false; //
+        int rollCount = 0;      //  Counts how many times roll has been clocked
+        //private SoundPlayer mPlayer = new SoundPlayer();    waiting to see if there actually is a sound component
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Preps the labels to have images
         private void Form1_Load(object sender, EventArgs e)
         {
             UI_DiceBox1.Image = null;
@@ -36,23 +43,37 @@ namespace CMPE1600_ICA6
             UI_DiceBox6.Image = null;
         }
 
+        //Starts the dice roll timers, and increments rolls
+        //updates the roll label
         private void UI_RollButton_Click(object sender, EventArgs e)
         {
             imageTimer.Enabled = true;
             rollTimer.Enabled = true;
             rollCount++;
-            
+            UI_RollValue.Text = rollCount.ToString();
         }
 
+        //On each tick, stores a random number in each die value
+        //unless there has been a hold placed on that die.
+        //Each label then accesses an image according to
+        //value stored at last tick
         private void imageTimer_Tick(object sender, EventArgs e)
         {
             Random rand = new Random();
-            dice1 = rand.Next(6);
-            dice2 = rand.Next(6);
-            dice3 = rand.Next(6);
-            dice4 = rand.Next(6);
-            dice5 = rand.Next(6);
-            dice6 = rand.Next(6);
+
+            if (dice1Hold == false)            
+                dice1 = rand.Next(6);            
+            if (dice2Hold == false)
+                dice2 = rand.Next(6);
+            if (dice3Hold == false)
+                dice3 = rand.Next(6);
+            if (dice4Hold == false)
+                dice4 = rand.Next(6);
+            if (dice5Hold == false)
+                dice5 = rand.Next(6);
+            if (dice6Hold == false)
+                dice6 = rand.Next(6);
+
             UI_DiceBox1.Image = imageList1.Images[dice1];
             UI_DiceBox2.Image = imageList1.Images[dice2];
             UI_DiceBox3.Image = imageList1.Images[dice3];
@@ -61,13 +82,159 @@ namespace CMPE1600_ICA6
             UI_DiceBox6.Image = imageList1.Images[dice6];
         }
 
+        //Controls length of time that dice roll
+        //On tick, stops itself and image timer
+        //Gets total of dice values
+        //Disables roll button on 3rd roll, enabled play again
         private void rollTimer_Tick(object sender, EventArgs e)
         {
             imageTimer.Enabled = false;
             rollTimer.Enabled = false;
             UI_ScoreValue.Text = (((dice1 + 1) + (dice2 + 1) + (dice3 + 1) + (dice4 + 1) + (dice5 + 1) + (dice6 + 1)).ToString());
+            if (rollCount > 2)
+            {
+                UI_ScoreLabel.Text = "Final Score: ";
+                UI_PlayAgainButton.Enabled = true;
+                UI_RollButton.Enabled = false;
+            }
+        }
 
+        //Resets everything to base case
+        private void UI_PlayAgainButton_Click(object sender, EventArgs e)
+        {
+            UI_RollButton.Enabled = true;
+            rollCount = 0;
+            UI_RollValue.Text = 0.ToString();
+            UI_ScoreLabel.Text = "Score:";
+            UI_ScoreValue.Text = 0.ToString();
+            UI_DiceBox1.BackColor = Color.White;
+            dice1Hold = false;
+            UI_DiceBox2.BackColor = Color.White;
+            dice2Hold = false;
+            UI_DiceBox3.BackColor = Color.White;
+            dice3Hold = false;
+            UI_DiceBox4.BackColor = Color.White;
+            dice4Hold = false;
+            UI_DiceBox5.BackColor = Color.White;
+            dice5Hold = false;
+            UI_DiceBox6.BackColor = Color.White;
+            dice6Hold = false;
+            UI_DiceBox1.Image = null;
+            UI_DiceBox2.Image = null;
+            UI_DiceBox3.Image = null;
+            UI_DiceBox4.Image = null;
+            UI_DiceBox5.Image = null;
+            UI_DiceBox6.Image = null;
 
+        }
+        
+        //      All events below detect click on die, and change
+        //      border color based on previous state, as well as
+        //      either puts a hold on die or not depending on
+        //      previous state
+        private void UI_DiceBox1_MouseClick(object sender, MouseEventArgs e)
+        {            
+            if (rollCount != 0)
+            {
+                
+                if ((UI_DiceBox1.BackColor == Color.White) || (UI_DiceBox1.BackColor == Color.Green))
+                {
+                    UI_DiceBox1.BackColor = Color.Red;
+                    dice1Hold = true;
+                }
+                else if (UI_DiceBox1.BackColor == Color.Red)
+                {
+                    UI_DiceBox1.BackColor = Color.Green;
+                    dice1Hold = false;
+                }
+            }
+        }
+        private void UI_DiceBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rollCount != 0)
+            {
+
+                if ((UI_DiceBox2.BackColor == Color.White) || (UI_DiceBox2.BackColor == Color.Green))
+                {
+                    UI_DiceBox2.BackColor = Color.Red;
+                    dice2Hold = true;
+                }
+                else if (UI_DiceBox2.BackColor == Color.Red)
+                {
+                    UI_DiceBox2.BackColor = Color.Green;
+                    dice2Hold = false;
+                }
+            }
+        }
+        private void UI_DiceBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rollCount != 0)
+            {
+
+                if ((UI_DiceBox3.BackColor == Color.White) || (UI_DiceBox3.BackColor == Color.Green))
+                {
+                    UI_DiceBox3.BackColor = Color.Red;
+                    dice3Hold = true;
+                }
+                else if (UI_DiceBox3.BackColor == Color.Red)
+                {
+                    UI_DiceBox3.BackColor = Color.Green;
+                    dice3Hold = false;
+                }
+            }
+        }
+        private void UI_DiceBox4_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rollCount != 0)
+            {
+
+                if ((UI_DiceBox4.BackColor == Color.White) || (UI_DiceBox4.BackColor == Color.Green))
+                {
+                    UI_DiceBox4.BackColor = Color.Red;
+                    dice4Hold = true;
+                }
+                else if (UI_DiceBox4.BackColor == Color.Red)
+                {
+                    UI_DiceBox4.BackColor = Color.Green;
+                    dice4Hold = false;
+                }
+            }
+        }
+
+        private void UI_DiceBox5_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rollCount != 0)
+            {
+
+                if ((UI_DiceBox5.BackColor == Color.White) || (UI_DiceBox5.BackColor == Color.Green))
+                {
+                    UI_DiceBox5.BackColor = Color.Red;
+                    dice5Hold = true;
+                }
+                else if (UI_DiceBox5.BackColor == Color.Red)
+                {
+                    UI_DiceBox5.BackColor = Color.Green;
+                    dice5Hold = false;
+                }
+            }
+        }
+
+        private void UI_DiceBox6_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rollCount != 0)
+            {
+
+                if ((UI_DiceBox6.BackColor == Color.White) || (UI_DiceBox6.BackColor == Color.Green))
+                {
+                    UI_DiceBox6.BackColor = Color.Red;
+                    dice6Hold = true;
+                }
+                else if (UI_DiceBox6.BackColor == Color.Red)
+                {
+                    UI_DiceBox6.BackColor = Color.Green;
+                    dice6Hold = false;
+                }
+            }
         }
     }
 }
