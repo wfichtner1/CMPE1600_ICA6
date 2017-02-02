81fe9c47e2b054904b24f9576d8f3d1eb950717e
William Fichtner <william_25_35@hotmail.com>
2017-02-02 12:46:09 -0700

    enter key rolls

--- CMPE1600_ICA6/Form1.Designer.cs
+++ CMPE1600_ICA6/Form1.Designer.cs
@@ -186,6 +186,7 @@
             // 
             // Form1
             // 
+            this.AcceptButton = this.UI_RollButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 303);
